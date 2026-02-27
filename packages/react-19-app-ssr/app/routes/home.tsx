c0c909d77633447ea25b38d7e67bb7363e958b77
Livan.pro <me@livan.pro>
2026-02-27 19:12:56 +0000

    chore: remove unnecessary code

--- packages/react-19-app-ssr/app/routes/home.tsx
+++ packages/react-19-app-ssr/app/routes/home.tsx
@@ -15,15 +15,9 @@ export default function Home() {
   return (
     <>
       <h1>React Router</h1>
-      <div className="card">
+      <div>
         <button onClick={() => ++state.count}>count is {snap.count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
